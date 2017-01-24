b9a3a8f12d48ececcd8b32e5e1a8ad5276cfab19
BrandonLRobinson1 <BrandonLRobinson1@gmail.com>
2017-01-23 17:44:39 -0800

    ii  Please enter the commit message for your changes. Lines starting

---
+++ sprint-one/src/functional/stack.js
@@ -0,0 +1,32 @@
+var Stack = function() {
+  var someInstance = {};
+
+  // Use an object with numeric keys to store values
+  //var storage = {};
+  var count = 0;
+
+  // Implement the methods below
+  someInstance.push = function(value) {
+    count++;
+    this[count] = value;
+  };
+
+  someInstance.pop = function() {
+    var deleted = this[count];
+    //delete this.count;
+    count--;
+    console.log(deleted)
+    return deleted; 
+  };
+
+  someInstance.size = function() {
+    if ( count < 0 ) {
+      return 0;
+    }
+    return count;
+  };
+
+  return someInstance;
+
+};
+
