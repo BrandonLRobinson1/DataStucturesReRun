34a17ba497d90b60422fef1741dd9168044c261e
BrandonLRobinson1 <BrandonLRobinson1@gmail.com>
2017-02-01 15:10:35 -0800

    # Please enter the commit message for your changes. Lines starting

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,7 +1,6 @@
 var Stack = () => {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var stackProperties = Object.create( stackMethods );
   stackProperties.count = 0;
 
   return stackProperties;
@@ -15,7 +14,8 @@ stackMethods.push = ( value ) => {
   console.log( this.count );
   // console.log( stackMethods );
 }
+  var stackProperties = Object.create( stackMethods );
 
 // var tony = Stack()
 // console.log( tony.push );
-// console.log( Stack );
+// console.log( Stack ); 
