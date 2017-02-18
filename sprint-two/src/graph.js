48dcbbf72df93e7e4747e371dcfd30628f3b83c2
BrandonLRobinson1 <BrandonLRobinson1@gmail.com>
2017-02-17 17:05:42 -0800

    continuing with graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,6 +2,8 @@
 
 // Instantiate a new graph
 var Graph = function() {
+
+  this.storage = {};
   // this.count = 1;
   // this.prev = null;
   // this.next = null;
@@ -11,17 +13,18 @@ var Graph = function() {
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  this.node = node;
-  this.edge1 = null;
-  this.edge2 = null;
+  this.storage[node] = { value: node };
+  // this['node'].node = node;
+  this.storage[node].edge1 = null;
+  this.storage[node].edge2 = null;
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  
-  for ( var key in this ){
 
-    if (this[key] === node) {
+  for ( var key in this ){
+  //console.log(key, node, this.storage[node].value,  ' this line 26')
+    if (this.storage[node].value === node) {
       return true;
     } else {
       return false;
@@ -34,26 +37,49 @@ Graph.prototype.contains = function(node) {
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
 
-  for ( var key in this ){
-
-    if (this[key] === node) {
-      console.log(this[key], ' node before removal')
-      delete this[key];
-      console.log(this[key], ' node after removal')
+  for ( var key in this ) {
+    console.log(this.storage[node].value, node)
+    if (this.storage[node].value === node) {
+      console.log('mmyes')
+      console.log(this.storage[node].value, ' node before removal')
+      console.log(this.storage, this.storage[node])
+      delete this.storage[node];
+      console.log(this.storage[node].value, ' node after removal')
     }
-
   
   }  
-
 
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+
+  // for(var key in this){
+
+  //   if (this[key] === fromNode) {
+  //     if (this[key].edge1 === toNode || this[key].edge2 === toNode) {
+  //       return true
+  //     } else {
+  //       return false
+  //     }
+  //   }
+
+  // }
+
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+
+  // for(var key in this){
+    
+  //   if (this[key] === fromNode) {
+  //     this[key].edge1 = toNode;
+  //     console.log(this)
+  // }
+
+// }
+
 };
 
 // Remove an edge between any two specified (by value) nodes.
