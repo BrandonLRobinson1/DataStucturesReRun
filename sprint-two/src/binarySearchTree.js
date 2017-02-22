27d66f839eaec221ae20a5d22b178b7d8e7c6f90
BrandonLRobinson1 <BrandonLRobinson1@gmail.com>
2017-02-21 16:30:11 -0800

    methods added and working

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,21 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = [];
+  this.right = [];
 };
 
+BinarySearchTree.prototype.insert = () => {};
+BinarySearchTree.prototype.contains = () => {};
+BinarySearchTree.prototype.depthFirstLog = () => {};
+
+// BinarySearchTree.insert = () => {};
+// BinarySearchTree.contains = () => {};
+// BinarySearchTree.depthFirstLog = () => {};
+
+console.log(BinarySearchTree, ' tree')
+var test = BinarySearchTree(5);
+console.log(BinarySearchTree(5), ' tree2')
+console.log(test.insert)
 
 /*
  * Complexity: What is the time complexity of the above functions?
